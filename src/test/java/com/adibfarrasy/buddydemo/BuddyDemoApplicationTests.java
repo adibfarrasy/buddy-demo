9705b17861787e7b2a2276431da6292d5681adb5
adibfarrasy <adibfarrasy@gmail.com>
2022-04-17 15:39:49 +0700

    Initial commit

---
+++ src/test/java/com/adibfarrasy/buddydemo/BuddyDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adibfarrasy.buddydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuddyDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
